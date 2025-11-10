87d1b5622064d8385b96e5417c3556cc139bdd27
Beat Brändli <beat.braendli@bluewin.ch>
2025-11-10 17:30:48 +0100

    Improve screen rendering
    Implement:
        terminal
        kernel logs
        format strings
    
    Signed-off-by: Beat Brändli <beat.braendli@bluewin.ch>

--- kernel_logs/kernel_logger.cpp
+++ kernel_logs/kernel_logger.cpp
@@ -9,6 +9,10 @@ void kernel_logger::vlog(const char *fmt, va_list args, bool send_terminal) {
     format_string(fmt, output, MAX_TEXT_LENGTH, args);
     if (log_terminal && send_terminal) log_terminal->printf("%s", output);
     uart_send(output, MAX_TEXT_LENGTH);
+<<<<<<< HEAD
+    uart_send("\n",3);
+=======
+>>>>>>> c193a9f (Improve screen rendering)
 }
 
 void kernel_logger::init_terminal(terminal *terminal)
@@ -16,11 +20,19 @@ void kernel_logger::init_terminal(terminal *terminal)
     klogger.log_terminal = terminal;
 }
 
+<<<<<<< HEAD
 void kernel_logger::log(const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
     klogger.vlog(fmt, args, true);
+=======
+void kernel_logger::log(const char *fmt, bool send_terminal, ...)
+{
+    va_list args;
+    va_start(args, send_terminal);
+    klogger.vlog(fmt, args, send_terminal);
+>>>>>>> c193a9f (Improve screen rendering)
     va_end(args);
 }
 
