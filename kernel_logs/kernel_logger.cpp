81ef11761a665f7ce22512aa39ccde54c437d6a7
Beat Brändli <beat.braendli@bluewin.ch>
2026-03-27 14:44:56 +0100

    kernel_logs: create no terminal log function
    Create a logging function for C that logs over
    UART but not to the terminal. This function will be
    used, to prevent circular dependencies.
    
    Signed-off-by: Beat Brändli <beat.braendli@bluewin.ch>

--- kernel_logs/kernel_logger.cpp
+++ kernel_logs/kernel_logger.cpp
@@ -4,15 +4,13 @@
 
 kernel_logger kernel_logger::klogger;
 
+
 void kernel_logger::vlog(const char *fmt, va_list args, bool send_terminal) {
     char output[MAX_TEXT_LENGTH];
     format_string(fmt, output, MAX_TEXT_LENGTH, args);
     if (log_terminal && send_terminal) log_terminal->printf("%s", output);
     uart_send(output, MAX_TEXT_LENGTH);
-<<<<<<< HEAD
     uart_send("\n",3);
-=======
->>>>>>> c193a9f (Improve screen rendering)
 }
 
 void kernel_logger::init_terminal(terminal *terminal)
@@ -20,19 +18,11 @@ void kernel_logger::init_terminal(terminal *terminal)
     klogger.log_terminal = terminal;
 }
 
-<<<<<<< HEAD
 void kernel_logger::log(const char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
     klogger.vlog(fmt, args, true);
-=======
-void kernel_logger::log(const char *fmt, bool send_terminal, ...)
-{
-    va_list args;
-    va_start(args, send_terminal);
-    klogger.vlog(fmt, args, send_terminal);
->>>>>>> c193a9f (Improve screen rendering)
     va_end(args);
 }
 
@@ -44,6 +34,13 @@ kernel_logger &kernel_logger::get_logger()
 extern "C" void kernel_logger_log(const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
-    kernel_logger::get_logger().vlog(fmt, args);
+    kernel_logger::get_logger().vlog(fmt, args, true);
+    va_end(args);
+}
+
+extern "C" void kernel_logger_log_no_terminal(const char *fmt, ...) {
+    va_list args;
+    va_start(args, fmt);
+    kernel_logger::get_logger().vlog(fmt, args, false);
     va_end(args);
 }
